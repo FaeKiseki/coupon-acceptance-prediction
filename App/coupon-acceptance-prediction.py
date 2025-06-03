a25e578fea6c1b14fb8a15acd691bb67bd100bf9
Esméralda Nguetse <167350522+FaeKiseki@users.noreply.github.com>
2025-06-03 11:20:39 +0100

    Update coupon-acceptance-prediction.py

--- App/coupon-acceptance-prediction.py
+++ App/coupon-acceptance-prediction.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import joblib
-import numpy as np
+import pandas as pd
 import os
 
 # Model path relative to the app folder
@@ -18,36 +18,84 @@ based on contextual information such as weather, time, passenger, age, and tempe
 """)
 
 # --- User Input ---
-weather_options = ['Sunny', 'Rainy', 'Snowy']
+weather_options = ['Sunny', 'Snowy']
 time_options = ['7AM', '10AM', '2PM', '6PM', '10PM']
-passenger_options = ['Alone', 'Friends', 'Kids', 'Partner']
-age_options = ['below21', '21', '26', '31', '36', '41', '46', '50plus']
+passenger_options = ['Alone', 'Friend(s)', 'Kid(s)', 'Partner']
+age_options = ['below21', '26', '31', '36', '41', '46', '50plus']
 temperature_options = [30, 55, 80]
 
+temperature = st.selectbox('Temperature (°F)', temperature_options)
 weather = st.selectbox('Weather condition', weather_options)
 time = st.selectbox('Time of day', time_options)
 passenger = st.selectbox('Passenger type', passenger_options)
 age = st.selectbox('Age group', age_options)
-temperature = st.selectbox('Temperature (°F)', temperature_options)
 
-# --- Feature Encoding ---
-weather_map = {'Sunny': 0, 'Rainy': 1, 'Snowy': 2, 'Cloudy': 3}
-time_map = {'7AM': 0, '10AM': 1, '2PM': 2, '6PM': 3, '10PM': 4}
-passenger_map = {'Alone': 0, 'Friends': 1, 'Kids': 2, 'Partner': 3}
-age_map = {'below21': 0, '21': 1, '26': 2, '31': 3, '36': 4, '41': 5, '46': 6, '50plus': 7}
+# --- One-hot encoding ---
+input_dict = {
+    'temperature': temperature,
+    'has_children': 0,
+    'toCoupon_GEQ5min': 0,
+    'toCoupon_GEQ15min': 0,
+    'toCoupon_GEQ25min': 1,
+    'direction_same': 1,
+    'direction_opp': 0,
+    'destination_No Urgent Place': 0,
+    'destination_Work': 1
+}
 
-weather_encoded = weather_map[weather]
-time_encoded = time_map[time]
-passenger_encoded = passenger_map[passenger]
-age_encoded = age_map[age]
+# Encode passenger
+for val in ['Friend(s)', 'Kid(s)', 'Partner']:
+    input_dict[f'passanger_{val}'] = int(passenger == val)
 
-# Prepare the input array
-X_input = np.array([[weather_encoded, time_encoded, passenger_encoded, age_encoded, temperature]])
+# Encode weather
+for val in ['Snowy', 'Sunny']:
+    input_dict[f'weather_{val}'] = int(weather == val)
+
+# Encode time
+for val in ['7AM', '10AM', '2PM', '6PM', '10PM']:
+    input_dict[f'time_{val}'] = int(time == val)
+
+# Encode age
+for val in ['26', '31', '36', '41', '46', '50plus', 'below21']:
+    input_dict[f'age_{val}'] = int(age == val)
+
+# Fill missing categorical fields with 0
+extra_keys = [
+    'coupon_Carry out & Take away', 'coupon_Coffee House', 'coupon_Restaurant(20-50)', 'coupon_Restaurant(<20)',
+    'expiration_2h', 'gender_Male',
+    'maritalStatus_Married partner', 'maritalStatus_Single', 'maritalStatus_Unmarried partner', 'maritalStatus_Widowed',
+    'education_Bachelors degree', 'education_Graduate degree (Masters or Doctorate)', 'education_High School Graduate',
+    'education_Some High School', 'education_Some college - no degree'
+] + [
+    f'occupation_{job}' for job in [
+        'Arts Design Entertainment Sports & Media', 'Building & Grounds Cleaning & Maintenance', 'Business & Financial',
+        'Community & Social Services', 'Computer & Mathematical', 'Construction & Extraction',
+        'Education&Training&Library', 'Farming Fishing & Forestry', 'Food Preparation & Serving Related',
+        'Healthcare Practitioners & Technical', 'Healthcare Support', 'Installation Maintenance & Repair',
+        'Legal', 'Life Physical Social Science', 'Management', 'Office & Administrative Support', 'Personal Care & Service',
+        'Production Occupations', 'Protective Service', 'Retired', 'Sales & Related', 'Student',
+        'Transportation & Material Moving', 'Unemployed'
+    ]
+] + [
+    f'income_{inc}' for inc in [
+        '$12500 - $24999', '$25000 - $37499', '$37500 - $49999', '$50000 - $62499', '$62500 - $74999',
+        '$75000 - $87499', '$87500 - $99999', 'Less than $12500'
+    ]
+] + [
+    f'{place}_{freq}' for place in ['Bar', 'CoffeeHouse', 'CarryAway', 'RestaurantLessThan20', 'Restaurant20To50']
+    for freq in ['4~8', 'Less than 1', 'More than 8', 'never']
+]
+
+for key in extra_keys:
+    input_dict[key] = 0
+
+# Convert to DataFrame
+input_df = pd.DataFrame([input_dict])
 
 # --- Prediction ---
 if st.button("Predict Coupon Acceptance"):
-    prediction = model.predict(X_input)[0]
-    proba = model.predict_proba(X_input)[0][1]
+    prediction = model.predict(input_df)[0]
+    proba = model.predict_proba(input_df)[0][1]
 
     st.subheader("Prediction Result")
     st.markdown(f"**Coupon will be {'ACCEPTED' if prediction == 1 else 'DECLINED'}**")
